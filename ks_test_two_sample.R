dcedcd7651f5cf7a26fa435e4e9ec886c5c88c91
udaykiran177 <95631110+udaykiran177@users.noreply.github.com>
2022-03-02 10:53:22 +0530

    Update ks_test_two_sample.R

--- ks_test_two_sample.R
+++ ks_test_two_sample.R
@@ -14,3 +14,30 @@ if (n$p.value>l){
 }else{
   print("reject Ho")
 }
+
+/*output:
+
+"enter the values of x:"
+1: 59
+2: 68
+3: 44
+4: 71
+5: 63
+6: 46
+7: 69
+8: 54
+9: 48
+10: 
+Read 9 items
+[1] "enter the values of y:"
+1: 50
+2: 36
+3: 62
+4: 52
+5: 70
+6: 41
+7: 
+Read 6 items
+enter the LOS:0.01
+[1] "accept Ho"
+*/
