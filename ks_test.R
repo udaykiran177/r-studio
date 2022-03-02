1568ca05fe71446ef926d88ab7d84849e7b965fd
udaykiran177 <95631110+udaykiran177@users.noreply.github.com>
2022-03-02 10:54:33 +0530

    Update ks_test.R

--- ks_test.R
+++ ks_test.R
@@ -13,3 +13,20 @@ if (n$p.value>l){
 }else{
   print("reject Ho")
 }
+
+/*
+
+output:
+
+ "enter the values of x:"
+1: 0.24
+2: 0.49
+3: 0.57
+4: 0.63
+5: 0.76
+6: 0.89
+7: 
+Read 6 items
+enter the LOS:0.05
+[1] "accept Ho"
+*/
