3c0f6415e787db49296d4012714088f66a95ea5b
udaykiran177 <95631110+udaykiran177@users.noreply.github.com>
2022-03-02 10:58:05 +0530

    Update mann_witney_test.R

--- mann_witney_test.R
+++ mann_witney_test.R
@@ -13,3 +13,36 @@ if (n$p.value>l){
 }else{
   print("reject Ho")
 }
+
+/*
+
+output:
+
+"enter the values of x:"
+1: 2.1
+2: 4.0
+3: 6.3
+4: 5.4
+5: 4.8
+6: 3.7
+7: 6.1
+8: 3.3
+9: 
+Read 8 items
+[1] "enter the values of y:"
+1: 4.1
+2: 0.6
+3: 3.1
+4: 2.5
+5: 4.0
+6: 6.2
+7: 1.6
+8: 2.2
+9: 1.9
+10: 5.4
+11: 
+Read 10 items
+enter the LOS:0.05
+[1] "accept Ho"
+
+*/
